3aaa33f223ac999910c75ec2754a2e6cd32c93b2
Etienne Laurin <etienne@atnnn.com>
2016-02-20 20:02:48 +0000

    clean up

--- stream.sh
+++ stream.sh
@@ -4,22 +4,42 @@ set -eu -o pipefail
 
 STREAM_KEY=$(cat "$(dirname "$0")/key.secret")
 
-INRES="1366x768" # input resolution
-OUTRES="1366x768" # output resolution
-FPS="15" # target FPS
-GOP="30" # i-frame interval, should be double of FPS, 
-GOPMIN="15" # min i-frame interval, should be equal to fps, 
-THREADS="0" # max 6
-CBR="1000k" # constant bitrate (should be between 1000k - 3000k)
-QUALITY="ultrafast"  # one of the many FFMPEG preset
-# AUDIO_RATE="44100"
+INRES="1366x768"  # input resolution
+OUTRES=$INRES     # output resolution
+FPS="15"          # target FPS
+THREADS="4"       # max 6
+CBR="1000k"       # constant bitrate (should be between 1000k - 3000k)
+QUALITY="fast"    # or 'superfast', 'medium', 'slow', ...
+AUDIO_RATE="44100"
 SERVER="live"
+
+GOP=`expr $FPS \* 2` # i-frame interval, should be double of FPS, 
+GOPMIN=$FPS          # min i-frame interval, should be equal to FPS
+
+ffmpeg_opts="-thread_queue_size 512"
+
+screen=0:v
+screen_opts="-f x11grab -s $INRES -r $FPS -i :0.0"
+
+webcam=1:v
+webcam_opts="-f v4l2 -video_size 320x240 -framerate $FPS -i /dev/video0"
+
+filter="
+  [$screen]setpts=PTS-STARTPTS[bg];
+  [$webcam]setpts=PTS-STARTPTS,split=2[webcam1][webcam2];
+  [webcam1]edgedetect=high=0.3:low=0.2,smartblur[alpha];
+  [webcam2][alpha]alphamerge[fg];
+  [bg][fg]overlay=W-w:H-h[out]" 
+
+
 
 #        -f alsa -i pulse -ac 2 -ar $AUDIO_RATE 
 #  -acodec libmp3lame
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 \
-       -f flv \
-       -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
-       -s $OUTRES -preset $QUALITY -tune film -threads $THREADS -strict normal \
-       -bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY" 2>&1 | sed "s/$STREAM_KEY/**********/"
+#        -f alsa -ac 2 -i hw:0 \
+#  -map 2:a
+# -ac 2 -ar $AUDIO_RATE 
+
+ffmpeg $ffmpeg_opts $screen_opts $webcam_opts $audio_opts -filter_complex "$filter" -map "[out]" \
+    "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY" \
+  2>&1 | sed "s/$STREAM_KEY/**********/"
 
