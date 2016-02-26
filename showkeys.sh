ec93b78e9b46a844989109c4d922d40a5d791995
Etienne Laurin <etienne@atnnn.com>
2016-02-26 21:31:04 +0000

    turn repeated keys into dots

--- showkeys.sh
+++ showkeys.sh
@@ -4,21 +4,34 @@ set -eu
 set -o pipefail
 
 threshold=200 # milliseconds
+repeat_threshold=500 # milliseconds
 too_long=25 # characters
 
 TIMEFORMAT=%R
 
 elapsed=0
 accum=
+last=
+count=0
 
 swipl -F none -s showkeys.pl -t true -g main | \
     while true; do
-        if ! out=$({ time { read -r -t 0.$(printf "%03d" $((threshold-elapsed))) a && printf "%s\n" "$a"; }; } 2>&1); then
+        if [[ "$count" -gt 1 ]]; then
+            timeout_opts="-t 0.$repeat_threshold"
+        else
+            timeout_opts="-t 0.$(printf "%03d" $((threshold-elapsed)))"
+        fi
+        if ! out=$({ time { read -r $timeout_opts a && printf "%s\n" "$a"; }; } 2>&1); then
+            if [[ "$count" -gt 1 ]]; then
+                accum="${accum%.}$last"
+            fi
             if [[ -n "$accum" ]]; then
                 printf "%s\n" "$accum"
                 accum=
             fi
             elapsed=0
+            last=
+            count=0
             continue
         fi
         set -- $out
@@ -27,18 +40,28 @@ swipl -F none -s showkeys.pl -t true -g main | \
         duration=${duration/#0}
         duration=${duration/#0}
         duration=${duration/#0}
-        if [[ -n "$accum" ]]; then
+        if [[ -n "$accum" && "$count" -gt 1 ]]; then
             elapsed=$((elapsed+duration)) 
+        fi
+        if [[ "$last" = "$1" && $duration -le $repeat_threshold ]]; then
+            count=$((count+1))
+        else
+            if [[ "$count" -gt 1 ]]; then
+                accum="${accum%.}$last"
+            fi
+            last=$1
+            count=0
+        fi
+        if [[ "$count" -gt 1 ]]; then
+            accum="${accum/%$1 $1/$1.}."
+        else
+            accum="$accum $1" 
         fi
         # echo "elapsed:$elapsed duration:$duration" >&2
         if [[ $elapsed -gt $threshold  || ${#accum} -gt $too_long ]]; then
-            if [[ -n "$accum" ]]; then
-                printf "%s\n" "$accum"
-                accum=
-            fi
+            printf "%s\n" "$accum"
+            accum=
             elapsed=0
-        else
-            accum="$accum $1"
         fi
     done | \
     osd_cat -d 2 --age=1 -p top -A right -f "-*-fixed-*-*-*-*-40-*-*-*-*-*-*-*" -c orange -l 12 -s 2
--- stream.sh
+++ stream.sh
@@ -4,14 +4,16 @@ set -eu -o pipefail
 
 STREAM_KEY=$(cat "$(dirname "$0")/key.secret")
 
-INRES="1366x768"  # input resolution
+INRES=1366x768    # input resolution
 OUTRES=$INRES     # output resolution
-FPS="15"          # target FPS
-THREADS="6"       # max 6
-CBR="1000k"       # constant bitrate (should be between 1000k - 3000k)
-QUALITY="ultrafast" # or 'fast', 'medium', 'slow', ...
+FPS=15            # target FPS
+THREADS=6         # max 6
+CBR=1000k         # constant bitrate (should be between 1000k - 3000k)
+QUALITY=medium    # or 'ultrafast', 'superfast', 'fast', 'medium', 'slow'
 AUDIO_RATE=11025  # 44100
-SERVER="live"
+SERVER=live-jfk-2
+WEBCAM=off         # on or off
+AUDIO=off          # on or off
 
 GOP=`expr $FPS \* 2` # i-frame interval, should be double of FPS, 
 GOPMIN=$FPS          # min i-frame interval, should be equal to FPS
@@ -19,15 +21,20 @@ GOPMIN=$FPS          # min i-frame interval, should be equal to FPS
 screen=0:v
 screen_opts="-f x11grab -thread_queue_size 1024 -s $INRES -r $FPS -i :0.0"
 
-webcam=1:v
-webcam_opts="-f v4l2 -thread_queue_size 1024 -video_size 320x240 -framerate $FPS -i /dev/video0"
+if [[ $WEBCAM = off ]]; then
+  webcam_opts=
+  filter="[$screen]copy[out]"
+else
+  webcam=1:v
+  webcam_opts="-f v4l2 -thread_queue_size 1024 -video_size 320x240 -framerate $FPS -i /dev/video0"
 
-filter="
-  [$screen]setpts=PTS-STARTPTS[bg];
-  [$webcam]setpts=PTS-STARTPTS,split=2[webcam1][webcam2];
-  [webcam1]edgedetect=high=0.3:low=0.2,smartblur[alpha];
-  [webcam2][alpha]alphamerge[fg];
-  [bg][fg]overlay=W-w:H-h[out]" 
+  filter="
+    [$screen]setpts=PTS-STARTPTS[bg];
+    [$webcam]setpts=PTS-STARTPTS,split=2[webcam1][webcam2];
+    [webcam1]edgedetect=high=0.3:low=0.2,smartblur[alpha];
+    [webcam2][alpha]alphamerge[fg];
+    [bg][fg]overlay=W-w:H-h[out]"
+fi
 
 out_opts="
   -f flv
@@ -35,8 +42,13 @@ out_opts="
   -s $OUTRES -preset $QUALITY -threads $THREADS
   -bufsize $CBR"
 
-audio_in="-f alsa -thread_queue_size 1024 -ar $AUDIO_RATE -i hw:0"
-audio_out="-strict experimental -af highpass=f=300,lowpass=f=3000 -acodec aac -map 2:a"
+if [[ $AUDIO = off ]]; then
+  audio_in=
+  audio_out=
+else
+  audio_in="-f alsa -thread_queue_size 1024 -ar $AUDIO_RATE -i hw:0"
+  audio_out="-strict experimental -af highpass=f=300,lowpass=f=3000 -acodec aac -map 2:a"
+fi
 
 ffmpeg $screen_opts $webcam_opts $audio_in -filter_complex "$filter" -map "[out]" \
     $out_opts $audio_out "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY" \
