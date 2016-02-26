63e5e0584f68debd0972c39d6147936aebeacbf9
Etienne Laurin <etienne@atnnn.com>
2016-02-26 07:27:56 +0000

    improvements

--- stream.sh
+++ stream.sh
@@ -7,22 +7,20 @@ STREAM_KEY=$(cat "$(dirname "$0")/key.secret")
 INRES="1366x768"  # input resolution
 OUTRES=$INRES     # output resolution
 FPS="15"          # target FPS
-THREADS="4"       # max 6
+THREADS="6"       # max 6
 CBR="1000k"       # constant bitrate (should be between 1000k - 3000k)
-QUALITY="fast"    # or 'superfast', 'medium', 'slow', ...
-AUDIO_RATE="44100"
+QUALITY="ultrafast" # or 'fast', 'medium', 'slow', ...
+AUDIO_RATE=11025  # 44100
 SERVER="live"
 
 GOP=`expr $FPS \* 2` # i-frame interval, should be double of FPS, 
 GOPMIN=$FPS          # min i-frame interval, should be equal to FPS
 
-ffmpeg_opts="-thread_queue_size 512"
-
 screen=0:v
-screen_opts="-f x11grab -s $INRES -r $FPS -i :0.0"
+screen_opts="-f x11grab -thread_queue_size 1024 -s $INRES -r $FPS -i :0.0"
 
 webcam=1:v
-webcam_opts="-f v4l2 -video_size 320x240 -framerate $FPS -i /dev/video0"
+webcam_opts="-f v4l2 -thread_queue_size 1024 -video_size 320x240 -framerate $FPS -i /dev/video0"
 
 filter="
   [$screen]setpts=PTS-STARTPTS[bg];
@@ -31,15 +29,16 @@ filter="
   [webcam2][alpha]alphamerge[fg];
   [bg][fg]overlay=W-w:H-h[out]" 
 
+out_opts="
+  -f flv
+  -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p
+  -s $OUTRES -preset $QUALITY -threads $THREADS
+  -bufsize $CBR"
 
+audio_in="-f alsa -thread_queue_size 1024 -ar $AUDIO_RATE -i hw:0"
+audio_out="-strict experimental -af highpass=f=300,lowpass=f=3000 -acodec aac -map 2:a"
 
-#        -f alsa -i pulse -ac 2 -ar $AUDIO_RATE 
-#  -acodec libmp3lame
-#        -f alsa -ac 2 -i hw:0 \
-#  -map 2:a
-# -ac 2 -ar $AUDIO_RATE 
-
-ffmpeg $ffmpeg_opts $screen_opts $webcam_opts $audio_opts -filter_complex "$filter" -map "[out]" \
-    "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY" \
+ffmpeg $screen_opts $webcam_opts $audio_in -filter_complex "$filter" -map "[out]" \
+    $out_opts $audio_out "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY" \
   2>&1 | sed "s/$STREAM_KEY/**********/"
 
